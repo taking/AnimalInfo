96ec9384c8e29cc000b8f5eb541556cd9d1ebfb8
consine2 <consine2@gmail.com>
2022-08-03 17:08:58 +0900

    [220803] frontend - auth Service 개발, SignIn, SignUp APi 연동작업 완료

--- frontend/src/partials/Header.jsx
+++ frontend/src/partials/Header.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import AuthService from "../auth/authService";
 
 function Header() {
   const [top, setTop] = useState(true);
