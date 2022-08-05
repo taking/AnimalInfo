dd05d9bfd4458f8abbaa7ba48a1291901a3d92ef
hr1208 <hr1208@innogrid.com>
2022-08-05 17:08:11 +0900

    data crud 수정

--- backend-renew/src/utils/upload.utils.js
+++ backend-renew/src/utils/upload.utils.js
@@ -1,13 +1,21 @@
 var multer = require('multer');
+var fs = require('fs');
+const path = require('path')
+
+const dir = path.join(__dirname,'../file')
+
+
+
 
 storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, '/animal/animalinfo-develop/backend-renew/src/file')
+      cb(null, dir)
     },
     filename(req, file, cb) { // 파일명을 어떤 이름으로 올릴지
         cb( null, `${req.body.data_type}_${req.body.species}_${file.originalname}` );
   }
   });
+  if (!fs.existsSync(dir)) fs.mkdirSync(dir);
   
 // upload = multer({ storage: storage }).array('imgNoseFront');
 upload = multer({ storage: storage })
