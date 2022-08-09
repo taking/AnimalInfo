29d9ff39877a053b67632e50a7f58eeaca5727c5
consine2 <consine2@gmail.com>
2022-08-09 11:10:13 +0900

    [220809] 파일 업로드 및 처리 기능 업데이트

--- backend/src/routes/data.route.js
+++ backend/src/routes/data.route.js
@@ -2,13 +2,12 @@ const express = require('express');
 const router = express.Router();
 const dataController = require('../controllers/data.controller');
 const auth = require('../middleware/auth.middleware');
-const upload = require('../utils/upload.utils');
+// const upload = require('../utils/upload.utils');s
 const Role = require('../utils/userRoles.utils');
 const awaitHandlerFactory = require('../middleware/awaitHandlerFactory.middleware');
 
-
-const formData = require('express-form-data');
-router.use(formData.parse());
+const fileUpload = require('express-fileupload');
+router.use(fileUpload());
 
 const { createDataSchema, updateDataSchema } = require('../middleware/validators/dataValidator.middleware');
 
@@ -17,7 +16,7 @@ const { createDataSchema, updateDataSchema } = require('../middleware/validators
 router.get('/', auth(),awaitHandlerFactory(dataController.getAllData)); // localhost:3000/api/v1/data
 router.get('/', auth(),awaitHandlerFactory(dataController.getAllData)); // localhost:3000/api/v1/data
 router.get('/id/:id', auth(), awaitHandlerFactory(dataController.getDataById)); // localhost:3000/api/v1/data/id/1
-router.post('/', auth(), createDataSchema, upload, awaitHandlerFactory(dataController.createData)); // localhost:3000/api/v1/data
+router.post('/', auth(), createDataSchema, awaitHandlerFactory(dataController.createData)); // localhost:3000/api/v1/data
 router.patch('/id/:id', auth(Role.Admin), updateDataSchema, awaitHandlerFactory(dataController.updateData)); // localhost:3000/api/v1/data/id/1 , using patch for partial update
 router.delete('/id/:id', auth(Role.Admin), awaitHandlerFactory(dataController.deleteData)); // localhost:3000/api/v1/data/id/1
 
