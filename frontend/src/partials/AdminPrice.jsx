f7207ce0e69e4fd677d5b5b643f0b8b36a39225f
consine2 <consine2@gmail.com>
2022-08-09 18:06:38 +0900

    [220809] front minor update

--- frontend/src/partials/AdminPrice.jsx
+++ frontend/src/partials/AdminPrice.jsx
@@ -84,6 +84,7 @@ function AdminPrice() {
 		resizable: true,
     // editable: true,
     cellEditorPopup: true,
+    suppressSizeToFit: true,
     // flex: 1,
 		// filter: true,
 		// floatingFilter: true,
